08b0c9584c1d7f06a73bc9e3b3c88f47b3f1e33e
Razvan Opris <nicuopris11@gmail.com>
2026-01-28 13:21:01 +0200

    Add sitemap

---
+++ app/sitemap.ts
@@ -0,0 +1,15 @@
+import { MetadataRoute } from 'next';
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  const baseUrl = 'https://razvan.dev';
+  const currentDate = new Date();
+
+  return [
+    {
+      url: baseUrl,
+      lastModified: currentDate,
+      changeFrequency: 'monthly',
+      priority: 1,
+    },
+  ];
+}
